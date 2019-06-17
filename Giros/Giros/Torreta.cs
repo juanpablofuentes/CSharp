719134e963692e85604be0e6ea9816b59930c301
Juan Pablo Fuentes <51440192+juanpablofuentes@users.noreply.github.com>
2019-06-17 10:05:52 +0200

    Update Torreta.cs

--- Giros/Giros/Torreta.cs
+++ Giros/Giros/Torreta.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,16 +7,33 @@ using System.Threading.Tasks;
 
 namespace Giros
 {
-    class Torreta : IGiros
+    public class Torreta : IGiros
     {
+        private ArrayList movimientos;
+        public Torreta()
+        {
+            movimientos = new ArrayList();
+        }
         public string girarDerecha()
         {
+            movimientos.Add("D");
             return "Girando una torreta a la derecha";
         }
 
         public string girarIzquierda()
         {
+            movimientos.Add("I");
             return "Girando una torreta a la izquierda";
+        }
+        public int posicion()
+        {
+            int pos = 0;
+            foreach(string move in movimientos)
+            {
+                if (move == "D") { pos += 90; }
+                if (move == "I") { pos -= 90; }
+            }
+            return pos;
         }
     }
 }
