6bc3002cf6513882b38fe5840e1ddada8183c0ed
Juan Pablo Fuentes <51440192+juanpablofuentes@users.noreply.github.com>
2019-06-17 09:20:30 +0200

    Update Program.cs

--- ArrayList/Program.cs
+++ ArrayList/Program.cs
@@ -1,31 +1,78 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ArrayList
+namespace CollectionArrayList
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            //Crear un arrayList
             ArrayList arryList1 = new ArrayList();
+            //Añadir elementos uno a uno
             arryList1.Add(1);
-            arryList1.Add("Two");
+            arryList1.Add("Eva");
             arryList1.Add(3);
             arryList1.Add(4.5);
 
-            IList arryList2 = new ArrayList()
-{
-    100, 200
-};
-
-            //Adding entire collection using ArrayList.AdRange() method.
-            ////Note: IList does not contain AddRange() method.
+            ArrayList arryList2 = new ArrayList() { 100, 200 };
+          
+          //Añadir un rango
             arryList1.AddRange(arryList2);
+            //Lo mostramos con tipo
+            Console.WriteLine("Contenido del arrraylist");
+            foreach (var item in arryList1)
+            {
+                Console.WriteLine(item+" - "+item.GetType());
+            }
+            //Lo mostramos accediendo por índice
+            for(int i = 0; i < arryList1.Count; i++)
+            {
+                Console.WriteLine(arryList1[i]);
+            }
+            Console.WriteLine("---------------------");
+            arryList1.Insert(1, "Elemento añadido");
+            arryList1.Insert(2, 100);
+            displayArrayList(arryList1);
+            arryList1.InsertRange(2, arryList2);
+            displayArrayList(arryList1);
+            arryList1.Remove(100); //Elimina el elemento 100 (sólo uno)
+            Console.WriteLine("Elemento 100 eliminado (el primero)");
+            displayArrayList(arryList1);
+            while (arryList1.Contains(100)) { arryList1.Remove(100); } //Elimina el elemento 100 (todos)
+            displayArrayList(arryList1);
+
+            arryList1.RemoveAt(1); //Elimina el elemento 1 (basado en índice 0)
+            displayArrayList(arryList1);
+            arryList1.RemoveRange(3, 2);//Elimina dos elementos a partir de la posición 3
+            displayArrayList(arryList1);
+
+            arryList1.Sort(new comparador());//Ordena la lista. Utilizamos un comparador porque los tipos son diferentes
+            displayArrayList(arryList1);
+            Console.WriteLine(arryList1.IndexOf("Eva"));
+            Console.WriteLine(arryList1.BinarySearch("Eva",new comparador())); //Podemos usar Binarysearch porque está ordenado
+        }
+        static void displayArrayList(ArrayList lista)
+        {
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---------------------");
+        }
+        public class comparador : IComparer
+        {
+
+            // Calls CaseInsensitiveComparer.Compare with the parameters reversed.
+            int IComparer.Compare(Object x, Object y)
+            {
+                return (x.ToString().CompareTo(y.ToString()));
+            }
+
         }
     }
 }
