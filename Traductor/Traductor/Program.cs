d2c6e3290a206ba362c8f2d0d0bf534b2075d39e
Juan Pablo Fuentes <51440192+juanpablofuentes@users.noreply.github.com>
2019-06-20 12:09:42 +0200

    Add files via upload

---
+++ Traductor/Traductor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traductor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Translate t = new Translate();
+            t.addWord("red", "rojo");
+            t.addWord("blue", "azul");
+            t.addWord("green", "verde");
+            t.addWord("yellow", "amarillo");
+            t.addWord("white", "blanco");
+            Console.WriteLine(t.firstWord());
+            Console.WriteLine(t.search("white"));
+            Console.WriteLine(t.search("blue"));
+            t.removeTranslation("verde");
+            Console.WriteLine(t.search("green"));
+        }
+    }
+}
---
+++ Traductor/Traductor/Traductor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traductor
+{
+    class Translate
+    {
+        private SortedList words;
+        public Translate()
+        {
+            words = new SortedList();
+        }
+        public void addWord(string word, string translation)
+        {
+            words.Add(word, translation);
+        }
+        public string search(string word)
+        {
+            if (words.Contains(word))
+            {
+                return words[word].ToString();
+            }
+            else
+            {
+                return "";
+            }
+        }
+        public void removeWord(string word)
+        {
+            words.Remove(word);
+        }
+        public void removeTranslation(string translation)
+        {
+            int pos = words.IndexOfValue(translation);
+            if (pos >= 0)
+            {
+                words.RemoveAt(pos);
+            }
+        }
+        public string firstWord()
+        {
+            return words.GetByIndex(0).ToString();
+        }
+
+    }
+}
